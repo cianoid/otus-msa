a87d5a799b3103a65224647667dd9ddc412e0eef
Igor Shatava <cianoid@yandex.ru>
2026-07-25 15:27:21 +0300

    move

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
+TAG=$(date +%Y-%m-%d-%H%M)
+echo "Сборка образов"
+cd app || exit 1
+uv export --quiet --format requirements.txt --output-file requirements.txt
+docker build -q -t cianoid/otus-msa:"${TAG}" .
+
+echo "Загрузка образа cianoid/otus-msa:${TAG} в кубер"
+minikube image load cianoid/otus-msa:"${TAG}"
+cd ..
+#TAG=2026-07-25-1521
 echo "Установка приложения"
 helm upgrade app \
   ./chart \
@@ -5,6 +15,7 @@ helm upgrade app \
   --debug \
   --install \
   --wait \
-  --atomic \
+  --rollback-on-failure \
   --namespace default \
-  --values ./chart/values.yaml
+  --values ./chart/values.yaml \
+  --set image.tag="${TAG}"
